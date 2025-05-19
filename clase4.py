1deb7d43ee2997daa10f01b480e7fd929b2e4111
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-19 20:39:54 -0300

    Curso basico de python - Manejo de cadenas

---
+++ clase4.py
@@ -0,0 +1,18 @@
+name = "Philip"
+# print(type(name))
+print(name)
+
+dato = "Hola mundo"
+print(dato[5])
+# print(dato[2])
+# print(dato[7])
+# print(dato[-1])
+print(dato + ' ' + name)
+print(len(dato))
+print(dato.lower())
+print(dato.upper())
+print(dato.title())
+print(dato.replace('mundo', 'universo'))
+print(dato.split())
+print(dato.split('o'))
+print(dato.split('o', 1))
