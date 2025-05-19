190b8b5849b2af54bd09ce949f195b038a82df21
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-19 20:50:39 -0300

    Curso basico de python - enteros, flotantes y booleanos

---
+++ clase5.py
@@ -0,0 +1,36 @@
+x = 10
+y = 5.678
+# z = 1.2E6
+# a = 1.2E-6
+
+print(type(x))
+print(type(y))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+# print(z)
+# print(a)
+# print(type(x + y))
+# print(type(x - y))
+# print(type(x * y))
+# print(type(x / y))
+# print(type(x // y))
+# print(type(x % y))
+# print(type(x ** y))
+# print(type(x == y))
+# print(type(x != y))
+# print(type(x > y))
+# print(type(x < y))
+# print(type(x >= y))
+# print(type(x <= y))
+# print(type(x is y))
+# print(type(x is not y))
+# print(type(x in y))
+# print(type(x not in y))
+# print(type(x and y))
+# print(type(x or y))
+# print(type(not x))
+# print(type(x & y))
