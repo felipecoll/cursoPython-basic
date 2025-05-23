c5ffbe20004e0e106d268ed1a23de77fe156b5b0
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-23 19:56:47 -0300

    Iteracion y control de flujo con bucles en Python - 2

---
+++ clase12.py
@@ -0,0 +1,24 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# for i in numbers:
+#     print(i)
+#     if i == 5:
+#         break
+
+# for i in range(3, 10):
+#     print(i)
+#     if i == 5:
+#         break
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "cherry":
+        break
+
+x = 0 
+while x < 5:
+    print(x)
+    x += 1
+    if x == 3:
+        break
